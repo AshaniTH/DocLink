ac2a9054fca315dc15548da90d34b082720c0fde
Ashani <ashanitharukshi2002@gmail.com>
2025-10-01 18:58:26 +0530

    feat: implement user profile data loading and context integration in MyProfile component

--- frontend/src/pages/MyProfile.jsx
+++ frontend/src/pages/MyProfile.jsx
@@ -1,23 +1,14 @@
-import React, { useState } from "react";
-import { assets } from "../assets/assets";
+import React, { useContext, useState } from "react";
+
+import { AppContext } from "../context/AppContext";
 
 const MyProfile = () => {
-  const [userData, setUserData] = useState({
-    name: "HKM Gunawardhana",
-    image: assets.profile_pic,
-    email: "hkm@example.com",
-    phone: "1234567890",
-    address: {
-      line1: "123, Main Street",
-      line2: "Apt 4B",
-    },
-    gender: "Male",
-    dob: "1990-01-01",
-  });
+  const {userData,setUserData} = useContext(AppContext)
+  
 
   const [isEdit, setIsEdit] = useState(false);
 
-  return (
+  return userData &&(
     <div className="max-w-lg flex flex-col gap-2 text-sm">
       <h1>My Profile</h1>
       <img className="w-36 rounded" src={userData.image} alt="" />
