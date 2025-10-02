5a205c42cbca83228b3564184421f9235d3e5f8d
Ashani <ashanitharukshi2002@gmail.com>
2025-10-02 14:34:54 +0530

    feat: add appointment cancellation functionality with API integration

--- frontend/src/pages/MyAppointment.jsx
+++ frontend/src/pages/MyAppointment.jsx
@@ -3,16 +3,43 @@ import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
+
 const MyAppointment = () => {
 
-const{backendUrl,token}= useContext(AppContext)
+const{backendUrl,token, getDoctorsData}= useContext(AppContext)
 
 const [appointments,setAppointments] = useState([])
+const months = ['','Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+const slotDateformat = (slotdate) =>{
+  const dateArray = slotdate.split('_')
+  return dateArray['0']+ " " +months[Number(dateArray[1])] + " " +dateArray[2]
+}
 const getuserAppointments = async()=>{
   try{
     const {data} = await axios.get(backendUrl +'/api/user/appointments',{headers:{token}})
     if (data.success){
       setAppointments(data.appointments.reverse())
+    }
+
+  }
+  catch(error){
+    console.log(error)
+    toast.error(error.message)
+
+  }
+}
+
+const cancelAppointment = async(appointmentId) =>{
+  try{
+    const {data} = await axios.post(backendUrl +'/api/user/cancel-appointment',{appointmentId},{headers:{token}})
+    if (data.success){
+      toast.success(data.message)
+      getuserAppointments()
+      getDoctorsData()
+      
+    }else{
+      toast.error(data.message)
     }
 
   }
@@ -45,13 +72,14 @@ useEffect(()=>{
         <p className='text-zinc-700 font-medium mt-1'>Address:</p>
         <p className='text-xs' > {item.docData.address.line1}</p>
         <p className='text-xs'> {item.docData.address.line2}</p>
-        <p className='text-xs mt-1'><span className='text-5m text-neutral-700 font-medium'>Date & Time </span>{item.slotDate} | {item.slotTime}</p>
+        <p className='text-xs mt-1'><span className='text-5m text-neutral-700 font-medium'>Date & Time </span>{slotDateformat(item.slotDate)} | {item.slotTime}</p>
       </div>
       <div></div>
 
       <div className='flex flex-col gap-2 justify-center'>
-        <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'> Pay Online </button>
-        <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-500 hover:text-white transition-all duration-300'>Cancel Appointment</button>
+        {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300'> Pay Online </button>}
+        {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-500 hover:text-white transition-all duration-300'>Cancel Appointment</button>}
+        {item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500'>Appointment Cancelled</button>}
       </div>
 
     </div>
